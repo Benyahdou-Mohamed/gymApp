9537b86f4f8c0015e692208db01ce004a2e12f86
unknown <benyahdou.moh29@hotmail.com>
2024-01-13 23:00:44 +0100

    Add NAVBAR componnents and HOME,Excercice Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import {Route,Routes} from 'react-router-dom';
+import { Box } from '@mui/material';
+import {NavBar} from './components/NavBar'
+import {Home} from './pages/Home'
+import {ExerciceDetail} from './pages/ExerciceDetail'
 
 function App() {
-    return ( <div>
-        sssssssssssqdqssss </div>
+    return ( <Box >
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/exercice/:id" element={<ExerciceDetail/>}/>
+        </Routes>
+        
+        </Box>
     );
 }
 
