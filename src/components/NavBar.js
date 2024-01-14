faca61f0507ab3201e3f96b083a5e1455b2d1bd5
unknown <benyahdou.moh29@hotmail.com>
2024-01-14 19:05:36 +0100

    Add stack and link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,11 @@ import { NoEncryption } from '@mui/icons-material'
 
 export default function NavBar() {
   return (
-    <Stack>
+    <Stack 
+    direction="row"
+    justifyContent="space-around"
+    sx={{gap:{sm:'122px',xs:'40px'},mt:{sm:'32px',xs:'20px'}}}
+    >
       <Link to="/">
         <img src={Logo}/>
      
@@ -15,10 +19,12 @@ export default function NavBar() {
         direction="row"
         gap="40px"
         fontSize='24px'
+        alignItems="flex"
       >
-        <Link to='/' style={{textDecoration:'none',color:'#3A1212',borderBottom:'3px solid #ff2625'}}>Home</Link>
-        <a href="#exercises" style={{textDecoration:'none',color:'#3A1212'}}>Exercice</a>
+        <Link to='/' style={{textDecoration:'none',color:'#3A1212',borderBottom:'3px solid #ff2625'}}>Homes</Link>
+        <a href="#exercises" style={{textDecoration:'none',color:'#3A1212'}}>Exercices</a>
       </Stack>
+      
     </Stack>
 
   )
